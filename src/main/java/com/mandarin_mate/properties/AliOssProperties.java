3a2d58ff5074dd8b50731e5188f7455bafda1df8
Keepl <1431286186@qq.com>
2024-03-22 18:41:21 +0800

    引入oss对象存储服务，但未开发具体接口

--- src/main/java/com/mandarin_mate/properties/AliOssProperties.java
+++ src/main/java/com/mandarin_mate/properties/AliOssProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "alioss")
+@ConfigurationProperties(prefix = "mandarin-mate.alioss")
 @Data
 public class AliOssProperties {
 
